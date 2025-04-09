2b47b8bd074d5e3510d8376527e25781e9a90ff0
laura <deangeli.laura@iisgalvanimi.edu.it>
2025-04-09 09:17:55 +0000

    finito

---
+++ sprites1.js
@@ -0,0 +1,11 @@
+var running = [
+    "sprites/Run1.png",
+    "sprites/Run2.png",
+    "sprites/Run3.png",
+    "sprites/Run4.png",
+    "sprites/Run5.png",
+    "sprites/Run6.png",
+    "sprites/Run7.png",
+    "sprites/Run8.png",
+    "sprites/Run9.png"
+];
